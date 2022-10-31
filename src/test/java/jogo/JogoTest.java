483577ef5cb76766b03e47a5d5afed7f8ef4d5cd
Vânia Neves <vania.neves@gmail.com>
2022-10-31 17:57:44 -0300

    Initial commit

---
+++ src/test/java/jogo/JogoTest.java
@@ -0,0 +1,12 @@
+package jogo;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class JogoTest {
+
+}
