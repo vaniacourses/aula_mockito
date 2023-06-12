e77580ab4a212b523ecfe68f8febebef24fc1e30
Vânia Neves <vania.neves@gmail.com>
2023-06-12 17:36:06 -0300

    updating example

--- src/test/java/jogo/JogoTest.java
+++ src/test/java/jogo/JogoTest.java
@@ -1,12 +1,6 @@
 package jogo;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
-
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-
 public class JogoTest {
+	
 
 }
